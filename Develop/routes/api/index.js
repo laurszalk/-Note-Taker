782040bbe63536f366681925de22f03fa52e1c6e
laurszalk <laurenszalk@gmail.com>
2023-03-01 14:43:14 -0500

    fixed the post note function

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -3,15 +3,12 @@ const router = require("express").Router();
 //const uuid = require('uuid');
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
-
-// reference to our dataset
+let db = require("../../db/db.json");
 
 // ALL of these routes have the '/api' as a prefix
 // This route is looking for "/api/notes" w/ GET HTTP Method
 router.get("/notes", (req, res) => {
   //readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
-
-  const db = require("../../db/db.json");
 
   fs.readFile("./db/db.json", "utf-8", function (error, data) {
     if (error) {
@@ -22,33 +19,22 @@ router.get("/notes", (req, res) => {
     console.log(typeof data);
     res.json(db);
   });
-
-  /*fs.readFile("./db/db.json").then((data) => {
-    console.log(data);
-    res.json(JSON.parse(data))
-  });
-  */
-  // res.json(db);
 });
 
 router.post("/notes", (req, res) => {
+  // create temp data object
+  let newData = {
+    id: 12,
+    title: req.body.title,
+    text: req.body.text,
+  };
+  db.push(newData);
   fs.readFile("./db/db.json", "utf8", function (err, data) {
     if (err) throw err;
-    console.log(data); // this data is JSON
 
     // CONVERT THE DATA OBJECT TO A JS ARRAY
     let storedData = JSON.parse(data);
 
-    console.log(storedData); // this is an JS ARRAY
-    console.log(typeof storedData);
-
-    // create temp data object
-    let newData = {
-      id: 12,
-      title: req.body.title,
-      text: req.body.text,
-    };
-    // ADD DATA to
     storedData.push(newData);
 
     fs.writeFile("./db/db.json", JSON.stringify(storedData), function (err) {
@@ -56,9 +42,9 @@ router.post("/notes", (req, res) => {
 
       console.log("Success");
       // JUST SEND AN EMPTY RESPONSE --> THE CLIENT SIDE SHOULD THEN RETRIGGER A PAGE RELOAD AUTOMATICALLY
-      res.json();
     });
   });
+  res.json(db);
 });
 
 router.delete("/notes/:id", (req, res) => {
