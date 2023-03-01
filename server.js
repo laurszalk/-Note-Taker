357c3526c608849d7d895bd54c80233d6b1af083
laurszalk <laurenszalk@gmail.com>
2023-03-01 15:17:04 -0500

    moved files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const apiRoutes = require("./Develop/routes/api/index.js");
+const htmlRoutes = require("./Develop/routes/html/index.js");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.use("/api", apiRoutes); // configures all the routes
+
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
